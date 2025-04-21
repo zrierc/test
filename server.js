dbc0ec9942a706fdeaed92b99d33d820a737dc13
zrierc <68382685+zrierc@users.noreply.github.com>
2025-04-21 10:31:24 +0700

    init project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello workshop! Server jalan dengan NodeJS dan Express.");
+});
+
+app.get("/info", (req, res) => {
+  res.json({
+    name: "Simple Web Server",
+    version: "1.0.0",
+    uptime: process.uptime(),
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});

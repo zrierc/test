c64602e88f02308720d4d08416bc4606c676e4c7
zrierc <68382685+zrierc@users.noreply.github.com>
2025-04-21 16:05:40 +0700

    fix: change port to 8000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello workshop! Server jalan dengan NodeJS dan Express.");

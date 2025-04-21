fbdfa2c9e585056efc688ac6d7573e2ead5ad29b
zrierc <68382685+zrierc@users.noreply.github.com>
2025-04-21 12:10:33 +0700

    ci: change function name

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ import os
 
 
 def lambda_handler(event, context):
-    print(f"Hello Terraform from Lambda! Run in env: {os.environ['APP_ENV']}")
+    print(f"Hello Github Actions from Lambda! Run in env: {os.environ['APP_ENV']}")
     print(f"boto3 version: {boto3.__version__}")
     print(f"botocore version: {botocore.__version__}")
     print(f"event: {event}")
